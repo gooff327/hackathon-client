719aeffc922c2d8e8778d116fff61411e231ab26
gouwanfu <gouwanfu@kezaihui.com>
2020-10-26 15:15:05 +0800

    :sparkles: add login route.

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,9 +1,18 @@
 import { RouteProps } from 'react-router-dom'
 import { ReactElement } from 'react'
+import Login from '../pages/login';
 export interface Route extends RouteProps {
-  icon: ReactElement
+  icon?: ReactElement,
+  name: string
+  hide: boolean
 }
 
 const routes: Route[] = [
+  {
+    path: '/',
+    name: '登录',
+    component: Login,
+    hide: true
+  }
 ]
 export default routes
