8112f382cf781d1cbdc3b37eecb15635e6c4d875
zhangyabo <zhangyabo@kezaihui.com>
2020-10-30 15:53:07 +0800

    feat: userprofile

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -2,53 +2,60 @@ import React from 'react'
 import { gql } from 'apollo-boost'
 import { useQuery } from '@apollo/react-hooks'
 import { RouteComponentProps } from 'react-router'
+import { Avatar, Spin } from 'antd'
 import './index.scss'
 
 const GET_USER_INFO = gql`
     query getUserInfo {
         me {
+            _id
             name
             avatar
-            createdAt
             email
             verified
+            desc
         }
     }
 `
 
 const UserInfo = (props:any) => {
   const { loading, error, data } = useQuery(GET_USER_INFO)
-  console.log(loading, error, data)
   const { goToUserProfile } = props
-
   return (
-    <div className='user-info'>
-      <div className="avatar-con">
-        <img src="https://user-gold-cdn.xitu.io/2018/12/13/167a58bf6d68ece3?imageView2/1/w/180/h/180/q/85/format/webp/interlace/1" alt=""/>
-        <span onClick={goToUserProfile}>编辑</span>
-      </div>
-      <div className="info-con">
-        <h2 className='title'>暖刺</h2>
-        <div className='follow-block'>
-          <div className="follow-item">
-            <span className="num">0</span>
-            <span className="text">粉丝</span>
+    <Spin spinning={loading}>
+      {
+        data && <div className='user-info'>
+          <div className="avatar-con">
+            <Avatar
+              src={data.me.avatar}
+              className='avatar'
+              size={80}>{data.me.name.slice(0, 1)}</Avatar>
+            <span className='edit' onClick={goToUserProfile}>编辑</span>
           </div>
-          <div className="follow-item">
-            <span className="num">0</span>
-            <span className="text">关注</span>
-          </div>
-          <div className="follow-item">
-            <span className="num">0</span>
-            <span className="text">文章</span>
+          <div className="info-con">
+            <h2 className='title'>{data.me.name}</h2>
+            <div className='follow-block'>
+              <div className="follow-item">
+                <span className="num">0</span>
+                <span className="text">粉丝</span>
+              </div>
+              <div className="follow-item">
+                <span className="num">0</span>
+                <span className="text">关注</span>
+              </div>
+              <div className="follow-item">
+                <span className="num">0</span>
+                <span className="text">文章</span>
+              </div>
+            </div>
+            <div className="introduction">
+              <h4>个人简介</h4>
+              <span>{data.me.desc || '暂无'}</span>
+            </div>
           </div>
         </div>
-        <div className="introduction">
-          <h4>个人简介</h4>
-          <span>长路漫漫，唯剑作伴</span>
-        </div>
-      </div>
-    </div>
+      }
+    </Spin>
   )
 }
 
@@ -60,7 +67,6 @@ const MainContent = () => {
 
 const User = (props: RouteComponentProps) => {
   const goToUserProfile = () => {
-    console.log('goToUserProfile')
     props.history.push('/user/profile')
   }
 
